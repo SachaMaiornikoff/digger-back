dc89ecdeb4cfe4e4a468e83a6fecc182bb8c46c5
Sacha Maiornikoff <sacha.maiornikoff@predictice.com>
2020-01-20 10:11:54 +0100

    CORS update

--- src/main/java/com/smaiornikoff/back/CorsConfiguration.java
+++ src/main/java/com/smaiornikoff/back/CorsConfiguration.java
@@ -13,6 +13,6 @@ public class CorsConfiguration implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("GET", "POST")
-            .allowedOrigins("https://hardcore-yalow-d36656.netlify.com");
+            .allowedOrigins("https://digging-buddy.com", "http://localhost:8000");
     }
 }
